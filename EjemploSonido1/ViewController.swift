163df29d2203f501185d681b2e8ae827ededd7fa
aixpec <aixpec@gisystemsint.com>
2016-10-31 00:58:43 -0500

    Initial Commit

---
+++ EjemploSonido1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjemploSonido1
+//
+//  Created by Andrés Ixpec on 31/10/16.
+//  Copyright © 2016 Andrés Ixpec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
